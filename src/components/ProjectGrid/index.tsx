a3df5c7460f6cdf296b371d2a6485f44e9ac5dcc
Nathan Smith <nathan@nathansmith.io>
2018-07-25 21:52:44 -0400

    add modal

--- src/components/ProjectGrid/index.tsx
+++ src/components/ProjectGrid/index.tsx
@@ -1,26 +1,52 @@
 import * as React from 'react';
 import { css } from 'emotion';
+import Modal from 'react-modal';
+
 import ProjectGridItem from './ProjectGridItem';
 
-export default function ProjectGrid() {
-  return (
-    <section
-      className={css`
-        width: 100%;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-column-gap: 10px;
-        grid-row-gap: 15px;
-      `}
-    >
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-      <ProjectGridItem />
-    </section>
-  );
+class ProjectGrid extends React.Component<any, any> {
+  state = {
+    modalIsOpen: false,
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  render() {
+    return (
+      <>
+        <section
+          className={css`
+            width: 100%;
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-column-gap: 10px;
+            grid-row-gap: 15px;
+          `}
+        >
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+          <ProjectGridItem onClick={this.openModal} />
+        </section>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.closeModal}>Close Modal</button>
+        </Modal>
+      </>
+    );
+  }
 }
+
+export default ProjectGrid;

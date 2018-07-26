4aadd0c10dd136c83799e5a2cc535154490ec5ad
Nathan Smith <nathan@nathansmith.io>
2018-07-25 21:42:52 -0400

    darken on hover

--- src/components/ProjectGrid/ProjectGridItem.tsx
+++ src/components/ProjectGrid/ProjectGridItem.tsx
@@ -1,9 +1,14 @@
 import styled from 'react-emotion';
+import { darken } from 'polished';
 
 const ProjectGridItem = styled('div')`
   background-color: red;
   width: 100%;
   height: 100px;
+  &:hover {
+    cursor: pointer;
+    background-color: ${darken(0.2, 'red')};
+  }
 `;
 
 export default ProjectGridItem;

65b386136f9119186687cb870983139c88260f1d
Nathan Smith <nathan@nathansmith.io>
2018-08-03 23:41:40 -0400

    And typography.ts!

---
+++ src/utils/typography.ts
@@ -0,0 +1,19 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  googleFonts: [
+    {
+      name: 'Overpass',
+      styles: ['400', '700'],
+    },
+  ],
+  bodyFontFamily: [
+    'Overpass',
+    'Helvetica Neue',
+    'Helvetica',
+    'Futura',
+    'sans-serif',
+  ],
+})
+
+export default typography

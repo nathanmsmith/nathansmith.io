b2a8ecd52e8c1d509c6c196722d7638b2908ca65
Nathan Smith <nathan@nathansmith.io>
2018-08-05 12:48:15 -0400

    this works

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,10 +2,11 @@ import styled from 'react-emotion'
 
 export default styled('div')`
   max-width: 40rem;
-  margin: 2rem auto;
+  margin: 1rem auto;
+  padding: 0.6rem;
   font-size: 1.3rem;
 
-  @media (max-width: 480px) {
-    margin: 2rem 1rem;
+  @media (max-width: 340px) {
+    margin-top: 0.6rem;
   }
 `

704fa50801817663d76649269f83147c752038d8
Nathan Smith <nathan@nathansmith.io>
2018-08-05 14:08:16 -0400

    this works (#18)

--- src/components/PortraitPicker.tsx
+++ src/components/PortraitPicker.tsx
@@ -8,18 +8,27 @@ interface PortraitPickerProps {
 }
 
 export default function PortraitPicker(props: PortraitPickerProps) {
-  const image = props.images.edges[randomInt(0, props.images.edges.length)].node
+  const image = props.images[randomInt(0, props.images.length)].node
   return (
-    <Img
-      style={{ display: 'block' }}
-      className={css`
-        color: transparent;
-        margin: 2rem auto;
-        border-radius: 50%;
-        border: 12px solid rgba(0, 0, 0, 0.025);
-      `}
-      fixed={image.fixed}
-      alt="A photo of Nathan Smith."
-    />
+    <a href="/">
+      <Img
+        style={{ display: 'block' }}
+        className={css`
+          max-width: 320px;
+          max-height: 320px;
+          color: transparent;
+          margin: 0 auto 2rem;
+          border-radius: 50%;
+          border: 12px solid rgba(0, 0, 0, 0.025);
+
+          @media (max-width: 340px) {
+            margin-bottom: 1.4rem;
+          }
+        `}
+        fluid={image.fluid}
+        alt="A photo of Nathan Smith."
+        title="Click again?"
+      />
+    </a>
   )
 }

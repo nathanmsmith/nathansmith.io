ea85a44f50ff1a991c92a27a058e7a312453068f
Nathan Smith <nathan@nathansmith.io>
2019-03-30 21:26:39 -0400

    Blog support (#84)
    * Add a posts page
    
    * rough working version
    
    * Update post css a little
    
    * Post listing!
    
    * Add date to post
    
    * Draft support
    
    * make it work
    
    * update url
    
    * nope

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link as GLink } from 'gatsby'
 import { css } from '@emotion/core'
 import { transparentize } from 'polished'
 
@@ -13,8 +14,9 @@ export default function Link(props: LinkProps) {
     font-weight: bold;
     text-decoration: none;
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-    transition: color linear 0.1s, border-color linear 50ms,
-      padding-bottom linear 0.1s;
+    transition-duration: 50ms, 0.1s, 0.1s;
+    transition-timing-function: linear;
+    transition-property: border-color, color, padding-bottom;
     &:hover {
       color: ${transparentize(0.4, '#000')};
       border-bottom: transparent;
@@ -22,7 +24,13 @@ export default function Link(props: LinkProps) {
     }
   `
 
-  return (
+  const isRelative = props.href.startsWith('/')
+
+  return isRelative ? (
+    <GLink to={props.href} css={styles}>
+      {props.children}
+    </GLink>
+  ) : (
     <a href={props.href} css={styles}>
       {props.children}
     </a>
---
+++ src/pages/posts.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { css } from '@emotion/core'
+
+import Page from '../components/Page'
+import Link from '../components/Link'
+
+export const query = graphql`
+  query PostsQuery {
+    posts: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/posts/" } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default function Posts({ data }: any) {
+  const postsByYear = data.posts.edges.reduce((obj: any, { node }: any) => {
+    if (node.frontmatter.date && !node.frontmatter.draft) {
+      const date = new Date(node.frontmatter.date)
+      const year = date.getFullYear()
+      if (obj.hasOwnProperty(year)) {
+        obj[year].push(node)
+      } else {
+        obj[year] = [node]
+      }
+    }
+    return obj
+  }, {})
+
+  return (
+    <Page pageTitle="Posts">
+      <ul
+        css={css`
+          list-style: none;
+          margin: 0;
+        `}
+      >
+        <>
+          {Object.keys(postsByYear).map((year, i) => (
+            <>
+              <h3 key={i}>{year}</h3>
+              {postsByYear[year].map((post: any) => {
+                const { date, draft } = post.frontmatter
+                if (date && !draft) {
+                  return (
+                    <li>
+                      <time
+                        dateTime={date.toISOString()}
+                        css={css`
+                          display: block;
+                          color: #ccc;
+                          font-size: 1rem;
+                        `}
+                      >
+                        {date.toLocaleDateString(undefined, {
+                          month: 'long',
+                          day: 'numeric',
+                        })}
+                      </time>
+                      <Link href={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                    </li>
+                  )
+                }
+              })}
+            </>
+          ))}
+        </>
+      </ul>
+    </Page>
+  )
+}

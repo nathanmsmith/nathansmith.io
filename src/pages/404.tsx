97a2e5cbb59d168b8b06855e478ee8d4a6c3dddf
Nathan Smith <nathan@nathansmith.io>
2020-03-10 09:34:15 -0700

    Fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,9 +4,8 @@ import { graphql } from 'gatsby'
 import Page from '../components/Page'
 
 const FourOhFour = () => (
-  <Page pageTitle="Downtime">
+  <Page pageTitle="Page Not Found">
     <>
-      <h1>Page Not Found</h1>
       <p>
         whoops. idk what you were looking for, but it's not here. sorry. let's
         go back to the last page you were at and pretend this didn't happen?

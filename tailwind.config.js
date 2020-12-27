d6840684b98d47060b49d94dd68d0c45fe71109b
Nathan Smith <nathan@nathansmith.io>
2020-12-26 22:03:54 -0800

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,26 @@
 module.exports = {
+  darkMode: 'media',
   theme: {
     extend: {
-      colors: {
-        primary: 'var(--color-primary)',
-        primaryh: 'var(--color-primary-hover)',
-        background: 'var(--color-background)',
-        border: 'var(--color-border)',
-        linku: 'var(--color-link-underline)',
-      },
-      borderWidth: {
-        12: '12px',
-      },
-      screens: {
-        dark: { raw: '(prefers-color-scheme: dark)' },
-      },
-      inset: {
-        '1r': '1rem',
-        '2r': '2rem',
-        '20': '20px',
-        '80': '80px',
-        '108': '108px',
-      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: 'blue',
+          },
+        },
+        dark: {
+          css: {
+            // color: theme('colors.gray.700', defaultTheme.colors.gray[700]),
+            color: 'red',
+          },
+        },
+      }),
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }

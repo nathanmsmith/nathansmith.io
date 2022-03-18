7f86013367b5053762071ad6aff0786cc17b51b1
Nathan Smith <nathan@nathansmith.io>
2022-03-17 23:53:08 -0700

    Update dark theme, using tailwind typography's builtin dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,26 +53,6 @@ module.exports = {
             },
           },
         },
-        dark: {
-          css: {
-            color: theme("colors.gray.200"),
-            h1: {
-              color: theme("colors.gray.50"),
-            },
-            h2: {
-              color: theme("colors.gray.50"),
-            },
-            h3: {
-              color: theme("colors.gray.50"),
-            },
-            h4: {
-              color: theme("colors.gray.50"),
-            },
-            code: {
-              color: theme("colors.gray.50"),
-            },
-          },
-        },
       }),
     },
   },
